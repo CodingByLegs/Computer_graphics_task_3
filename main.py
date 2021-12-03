a1b6bc20b93890f97d43b228f3cc0d579b25d44d
Nikolai Groshev <kolia.avatar@gmail.com>
2021-12-04 01:29:46 +0300

    megaturbo alpha version -1.0

--- main.py
+++ main.py
@@ -20,16 +20,18 @@ def notch_reject_filter(shape, d0=9, u_k=0, v_k=0):
                 H[u, v] = 1.0
     return H
 
-def set_plot_title(title, fs = 16):
-    plt.title(title, fontsize = fs)
+
+def set_plot_title(title, fs=16):
+    plt.title(title, fontsize=fs)
+
 
 img = cv2.imread("Samples/Bbain_03_55.png", 0)
 
 f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
-phase_spectrumR = np.angle(fshift)
-magnitude_spectrum = 20*np.log(np.abs(fshift))
-cv2.imwrite("Samples/tmp.png",magnitude_spectrum)
+phase_spectrum = np.angle(fshift)
+magnitude_spectrum = 20 * np.log(np.abs(fshift))
+cv2.imwrite("Samples/tmp.png", magnitude_spectrum)
 
 img_shape = img.shape
 
@@ -40,19 +42,18 @@ H4 = notch_reject_filter(img_shape, 7, 15, -17.5)
 H5 = notch_reject_filter(img_shape, 10, 50, -62.5)
 H6 = notch_reject_filter(img_shape, 10, 30, -37.5)
 
-NotchFilter = H1*H2*H3*H4*H5*H6
+NotchFilter = H1 * H2 * H3 * H4 * H5 * H6
 NotchRejectCenter = fshift * NotchFilter
 NotchReject = np.fft.ifftshift(NotchRejectCenter)
 inverse_NotchReject = np.fft.ifft2(NotchReject)
 
-
 Result = np.abs(inverse_NotchReject)
 
-plt.imshow(Image.open(pathlib.Path("Samples/tmp.png")), cmap = "gray")
+plt.imshow(Image.open(pathlib.Path("Samples/tmp.png")), cmap="gray")
 set_plot_title("Click on image to choose points. (Press any key to Start)")
 plt.waitforbuttonpress()
 set_plot_title(f'Select 4 points with mouse click')
-points = np.asarray(plt.ginput(4) )
+points = np.asarray(plt.ginput(4))
 plt.close()
 
 print(points)
@@ -66,12 +67,11 @@ plt.imshow(magnitude_spectrum, cmap='gray')
 plt.title('magnitude spectrum')
 
 plt.subplot(223)
-plt.imshow(magnitude_spectrum*NotchFilter, "gray")
+plt.imshow(magnitude_spectrum * NotchFilter, "gray")
 plt.title("Notch Reject Filter")
 
 plt.subplot(224)
 plt.imshow(Result, "gray")
 plt.title("Result")
-
 
 plt.show()
